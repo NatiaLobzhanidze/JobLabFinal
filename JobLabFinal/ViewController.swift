ce90e5f757d0d4d2662fca87e50b7043ec62d59f
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-08-31 16:52:46 +0400

    Initial Commit

---
+++ JobLabFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobLabFinal
+//
+//  Created by Natia's Mac on 31.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
