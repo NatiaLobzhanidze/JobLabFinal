1ddb56e29bd645eedb251db12a84c0126d922f7f
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-11-09 12:38:57 +0400

    commmessage

--- JobLabFinal/API/APIManager.swift
+++ JobLabFinal/API/APIManager.swift
@@ -8,22 +8,30 @@
 import Foundation
 
 
-final class APIManager {
+public final class APIManager {
     
-    static var shared = APIManager()
-    private init() {}
+    public static var shared = APIManager()
+    init() {}
     
-    func fetchData<T: Decodable>(urlString: String, decodingType: T.Type) async throws -> T {
+    public  func fetchData<T: Decodable>(urlString: String, decodingType: T.Type) async throws -> T {
         let session = URLSession.shared
-        guard let url = URL(string: urlString) else { throw ApiError.invalidUrl }
+        
+        guard let url = URL(string: urlString) else {
+            
+            throw ApiError.invalidUrl }
+        
         let (data, response) = try await session.data(from: url)
-        guard let httpResponse = response as? HTTPURLResponse,
-        (200...299).contains(httpResponse.statusCode) else { throw ApiError.httpError }
+        guard let httpResponse = response as? HTTPURLResponse else { throw ApiError.unknown }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw ApiError.httpError(statusCode: httpResponse.statusCode)
+        }
+        
         do {
-           return try JSONDecoder().decode(decodingType.self, from: data)
+            return try JSONDecoder().decode(decodingType.self, from: data)
         } catch {
             print(error.localizedDescription)
-            throw ApiError.decodingError
+            throw ApiError.decodingError(error.localizedDescription)
         }
     }
 }
