a216bb3363a7e09a858501037831482fe3851dfb
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-11-12 13:15:17 +0400

    6 newC

---
+++ JobLabFinal/API/APIManager_BASE_13949.swift
@@ -0,0 +1,29 @@
+//
+//  APIManager.swift
+//  JobLabFinal
+//
+//  Created by Natia's Mac on 10.09.22.
+//
+
+import Foundation
+
+
+final class APIManager {
+    
+    static var shared = APIManager()
+    private init() {}
+    
+    func fetchData<T: Decodable>(urlString: String, decodingType: T.Type) async throws -> T {
+        let session = URLSession.shared
+        guard let url = URL(string: urlString) else { throw ApiError.invalidUrl }
+        let (data, response) = try await session.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse,
+        (200...299).contains(httpResponse.statusCode) else { throw ApiError.httpError }
+        do {
+           return try JSONDecoder().decode(decodingType.self, from: data)
+        } catch {
+            print(error.localizedDescription)
+            throw ApiError.decodingError
+        }
+    }
+}
---
+++ JobLabFinal/API/APIManager_REMOTE_13949.swift
@@ -0,0 +1,37 @@
+//
+//  APIManager.swift
+//  JobLabFinal
+//
+//  Created by Natia's Mac on 10.09.22.
+//
+
+import Foundation
+
+
+public final class APIManager {
+    
+    public static var shared = APIManager()
+    init() {}
+    
+    public  func fetchData<T: Decodable>(urlString: String, decodingType: T.Type) async throws -> T {
+        let session = URLSession.shared
+        
+        guard let url = URL(string: urlString) else {
+            
+            throw ApiError.invalidUrl }
+        
+        let (data, response) = try await session.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse else { throw ApiError.unknown }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw ApiError.httpError(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(decodingType.self, from: data)
+        } catch {
+            print(error.localizedDescription)
+            throw ApiError.decodingError(error.localizedDescription)
+        }
+    }
+}
